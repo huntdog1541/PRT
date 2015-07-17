48a39f41964c5bb2639c531af307999cced47a08
David Hunt <huntdog1541@gmail.com>
2015-07-17 01:09:26 -0500

    Create Graphic.java

---
+++ Graphic.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by David on 7/17/2015.
+ */
+public class Graphic {
+
+    public JFrame frame;
+    private TxtContent content;
+
+    public Graphic()
+    {
+        frame = new JFrame();
+        content = new TxtContent();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(500, 600));
+        frame.setVisible(true);
+        frame.add(content.getTextArea());
+    }
+
+
+}
