25a9109e92552534a77389ed4d5fba2aaecf8b37
David Hunt <huntdog1541@gmail.com>
2015-07-17 01:09:41 -0500

    Create TxtContent.java

---
+++ TxtContent.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+
+/**
+ * Created by David on 7/17/2015.
+ */
+public class TxtContent {
+
+    private String prompt;
+    private String comd;
+    private char[] storeCommand;
+    private char[] storeTemp;
+    private JTextArea area;
+
+    public TxtContent()
+    {
+        area = new JTextArea();
+    }
+
+    public JTextArea getTextArea()
+    {
+        return area;
+    }
+
+    public boolean startPrompt()
+    {
+        boolean ans = false;
+
+        return ans;
+    }
+
+    public boolean printPrompt()
+    {
+        boolean ans = false;
+
+        return ans;
+    }
+
+
+
+}
